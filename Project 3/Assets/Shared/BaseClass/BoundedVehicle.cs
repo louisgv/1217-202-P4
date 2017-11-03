63b3459de144ecf56c23df6becf931bc042e25d6
Joe Johans <lgvichy@gmail.com>
2017-11-03 16:27:31 -0400

    Bounce steering seems ok so far

--- Project 3/Assets/Shared/BaseClass/BoundedVehicle.cs
+++ Project 3/Assets/Shared/BaseClass/BoundedVehicle.cs
@@ -36,21 +36,39 @@ public abstract class BoundedVehicle : Vehicle
 		var minBound = boundingPlane.GetMinBound ();
 		var maxBound = boundingPlane.GetMaxBound ();
 
-		var desiredDirection = Direction;
+//		Vector3 desiredDirection = Direction;
+//
+//		if (transform.position.x > maxBound.x || transform.position.x < minBound.x) {
+//			desiredDirection.x *= -1.0f;
+//		} else if (transform.position.z > maxBound.z || transform.position.z < minBound.z) {
+//			desiredDirection.z *= -1.0f;
+//		}
+//
+//		if (desiredDirection.Equals (Direction)) {
+//			return Vector3.zero;
+//		}
+//
+//		var desiredVelocity = desiredDirection.normalized * boundingParams.MaxSpeed;
 
-		if (transform.position.x > maxBound.x || transform.position.x < minBound.x) {
-			desiredDirection.x *= -1.0f;
+		Vector3 desiredVelocity = Vector3.zero;
+
+		if (transform.position.x > maxBound.x) {
+			desiredVelocity = new Vector3 (-boundingParams.MaxSpeed, 0, Velocity.z);
+		} else if (transform.position.x < minBound.x) {
+			desiredVelocity = new Vector3 (boundingParams.MaxSpeed, 0, Velocity.z);
+		} 
+
+		if (transform.position.z > maxBound.z) {
+			desiredVelocity = new Vector3 (Velocity.x, 0, -boundingParams.MaxSpeed);
+		} else if (transform.position.z < minBound.z) {
+			desiredVelocity = new Vector3 (Velocity.x, 0, boundingParams.MaxSpeed);
 		}
 
-		if (transform.position.z > maxBound.z || transform.position.z < minBound.z) {
-			desiredDirection.z *= -1.0f;
-		}
-
-		if (desiredDirection.Equals (Direction)) {
+		if (desiredVelocity.Equals (Vector3.zero)) {
 			return Vector3.zero;
 		}
 
-		var desiredVelocity = desiredDirection.normalized * boundingParams.MaxSpeed;
+		desiredVelocity = desiredVelocity.normalized * boundingParams.MaxSpeed;
 
 		return SteeringForce.GetSteeringForce (this, desiredVelocity) * boundingParams.ForceScale;
 	}
